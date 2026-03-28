ea24865573bd43cf192c741cb92859770a90a4cc
j l <jlin5@ufl.edu>
2026-03-28 10:48:50 -0400

    launch/start screen, branding w fonts/colors, changed title font from Balonku to supertrend (wasn't consistent w original Figma mockup, looked really odd in render)

---
+++ script.js
@@ -0,0 +1,5 @@
+//screen switching
+function showScreen(id) {
+    document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
+    document.getElementById(id).classList.add('active');
+}
